31c98bcd073e90cee31dc419583dd8ee1c818e53
vojtechpavlu <vojtech.pavlu.dev@gmail.com>
2024-03-16 08:54:41 +0100

    Define tests for random numbers

---
+++ test/utils/randomFloat.test.ts
@@ -0,0 +1,29 @@
+import { randomFloat } from '../../src/utils';
+
+describe('randomFloat function', () => {
+  it('should return a number within range', () => {
+    const value = randomFloat(1, 2);
+    expect(value >= 1).toBeTruthy()
+    expect(value <= 2).toBeTruthy()
+  });
+
+  it('should return a number when min === max', () => {
+    const value = randomFloat(1, 1);
+    expect(value).toBe(1);
+  });
+
+  it('should return a specific decimal digits', () => {
+    const value = randomFloat(1, 5, 3);
+    const decimalPart = `${value}`.split(".")[1]!
+
+    expect(decimalPart.length).toBe(3)
+  })
+
+  it('should throw on min > max', () => {
+    expect(() => randomFloat(30, 1)).toThrow()
+  })
+
+  it('should throw on negative decimalDigits', () => {
+    expect(() => randomFloat(1, 5, -1)).toThrow()
+  })
+})
---
+++ test/utils/randomInteger.test.ts
@@ -0,0 +1,18 @@
+import { randomInteger } from '../../src/utils';
+
+describe('randomInteger function', () => {
+  it('should return a number within range', () => {
+    const value = randomInteger(1, 2);
+    expect(value >= 1).toBeTruthy()
+    expect(value <= 2).toBeTruthy()
+  });
+
+  it('should return a number when min === max', () => {
+    const value = randomInteger(1, 1);
+    expect(value).toBe(1);
+  });
+
+  it('should throw on min > max', () => {
+    expect(() => randomInteger(30, 1)).toThrow()
+  })
+})
