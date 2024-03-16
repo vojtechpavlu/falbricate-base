54938c92072c00d8df7810b33215711e0184a594
vojtechpavlu <vojtech.pavlu.dev@gmail.com>
2024-03-16 09:53:40 +0100

    Add charset tests

---
+++ test/utils/common/charsets/isCharset.test.ts
@@ -0,0 +1,34 @@
+import { isCharset } from '../../../../src/utils/common';
+
+describe('isCharset function', () => {
+  it('should return false on empty array', () => {
+    expect(isCharset([])).toBe(false);
+  })
+
+  it('should return false on non-array', () => {
+    expect(isCharset("test")).toBe(false);
+  })
+
+  it('should return false on undefined', () => {
+    expect(isCharset(undefined)).toBe(false);
+  })
+
+  it('should return false on null', () => {
+    expect(isCharset(null)).toBe(false);
+  })
+
+  it('should return false on non-single-character strings', () => {
+    const array = ['non-single', 'character']
+    expect(isCharset(array)).toBe(false);
+  })
+
+  it('should return false on non-string array', () => {
+    const array = [1, 2, 3]
+    expect(isCharset(array)).toBe(false);
+  })
+
+  it('should return true on single-character strings', () => {
+    const array = ['a', 'b', 'c']
+    expect(isCharset(array)).toBe(true);
+  })
+})
