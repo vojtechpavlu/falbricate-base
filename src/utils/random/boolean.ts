99484ede24c2c8ccff7c93e32f43c611c305dc6a
vojtechpavlu <vojtech.pavlu.dev@gmail.com>
2024-03-22 16:29:54 +0100

    Remove bug in boolean generator

--- src/utils/random/boolean.ts
+++ src/utils/random/boolean.ts
@@ -11,6 +11,6 @@ export const randomBoolean = (probability: number = 0.5) => {
   if (probability < 0 || probability > 1) {
     throw new Error(`Probability is expected to be in range of [0, 1]: ${probability}`)
   } else {
-    return randomFloat(0, 1) > probability
+    return randomFloat(0, 1) <= probability
   }
 }
