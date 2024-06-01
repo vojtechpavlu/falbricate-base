5db75d6bd3ea5b8c94576b3fc4b5ead77ec0f3f3
vojtechpavlu <vojtech.pavlu.dev@gmail.com>
2024-06-01 09:17:22 +0200

    Match UUIDs

--- src/profiles/generators/IdentifierProfileFabricator.ts
+++ src/profiles/generators/IdentifierProfileFabricator.ts
@@ -8,11 +8,17 @@ export class IdentifierProfileFabricator extends ProfileFabricator {
   private static readonly SCHEMA_INPUT: SchemaInput = {
     fields: {
       rowNumber: { type: 'context-input', config: { path: 'index' } },
-      uuid: 'uuid',
-      UUID: 'UUID',
       randH: { type: 'range-integer', config: { max: 100 } },
       randT: { type: 'range-integer', config: { max: 1_000 } },
       randM: { type: 'range-integer', config: { max: 1_000_000 } },
+      uuid: 'uuid',
+      UUID: {
+        type: 'context-input',
+        config: {
+          path: 'current.uuid',
+          pipes: ['uppercase']
+        },
+      }
     },
   };
 
